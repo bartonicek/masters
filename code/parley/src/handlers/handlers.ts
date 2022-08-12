b91a0ebed3decc530d07fe0d82915ee59c3bae60
Adam Bartonicek <bartonicek@gmail.com>
2022-08-13 03:32:40 +1200

    About to rework highlighting in a major way

--- code/parley/src/handlers/handlers.ts
+++ code/parley/src/handlers/handlers.ts
@@ -1,3 +1,5 @@
-export * from "./Handler.js";
-export * from "./RectDragHandler.js";
+export * from "./MarkerHandler.js";
 export * from "./KeypressHandler.js";
+export * from "./DragHandler.js";
+export * from "./StateHandler.js";
+export * from "./ClickHandler.js";
